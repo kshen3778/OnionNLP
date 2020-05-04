35d51e91ae156983e068076c6cfed548aa9a1b0b
kshen3778 <kshen3778@gmail.com>
2020-05-04 10:37:42 -0400

    first

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,69 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text: string;
+  title: string;
+  url: string;
+  language: string;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.text = "";
+    this.title = "";
+  }
+
+  getArticle(){
+    console.log("URL: " + this.url);
+    console.log("Language: " + this.language);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+
+    let data = {
+      "url": this.url,
+      "language": this.language
+    }
+
+    this.http
+    .post("http://127.0.0.1:5000/get_article", data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    }).subscribe(
+      res => {
+        console.log(res);
+        this.title = res.title;
+        this.text = "Please Wait. Analyzing Text ... ";
+
+        //Summarization
+        let sum_data = {
+          "text": res.text,
+          "select_n": 5
+        }
+        this.http.post("http://127.0.0.1:5000/api", sum_data, {
+          headers: new HttpHeaders().set('Content-Type', 'application/json')
+        }).subscribe(res2 => {
+            this.text = res2
+
+          },
+          (err2: HttpErrorResponse) => {
+            console.log(err2.error);
+            console.log(err2.name);
+            console.log(err2.message);
+            console.log(err2.status);
+          }
+
+        );
+
+
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err.error);
+        console.log(err.name);
+        console.log(err.message);
+        console.log(err.status);
+      }
+    );
+  }
 
 }
